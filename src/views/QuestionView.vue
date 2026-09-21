46246a724d0f6f93b876eb10d436ef19229f4303
Schubbii <ciupke.silas@hotmail.com>
2026-09-21 11:04:07 +0200

    Dateien übertragen auf richtige namen

---
+++ src/views/QuestionView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="home">
+    <h1>Was koche ich heute?</h1>
+  </main>
+</template>
+
+<style scoped>
+.home {
+  padding-top: 3rem;
+}
+
+h1 {
+  font-size: 2.5rem;
+  font-weight: 800;
+  line-height: 1.15;
+  color: var(--color-heading);
+}
+</style>
