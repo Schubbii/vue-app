522eb0990e5ce39e2964c7b68b5e1049089f1a64
Levin <Levin_Lisica@outlook.de>
2026-09-21 11:54:21 +0200

    QuestionView Click Handling

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,11 +1,46 @@
 <script setup>
+import { ref } from "vue";
 import QuestionCardContent from "../components/QuestionCardContent.vue";
+
+const currentStep = ref(1);
+const selectedCuisine = ref(null);
+const selectedType = ref(null);
+
+function selectCuisine(cuisine) {
+  selectedCuisine.value = cuisine;
+  currentStep.value = 2;
+}
+
+function selectType(type) {
+  selectedType.value = type;
+  currentStep.value = 3;
+}
 </script>
 
 <template>
-  <h1>Welche Küche?</h1>
+  <div v-if="currentStep === 1">
+    <h1>Cuisine?</h1>
 
-  <div class="card-container">
-    <QuestionCardContent title="String" image="String" description="String" />
+    <div class="card-container">
+      <QuestionCardContent
+        title="String"
+        image="String"
+        description="String"
+        @select="selectCuisine(String)"
+      />
+    </div>
+  </div>
+
+  <div v-if="currentStep === 2">
+    <h1>Meat or Vegetarian?</h1>
+
+    <div class="card-container">
+      <QuestionCardContent
+        title="String"
+        image="String"
+        description="String"
+        @select="selectType(String)"
+      />
+    </div>
   </div>
 </template>
