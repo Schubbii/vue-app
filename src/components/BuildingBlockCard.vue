113efff60b3c15b647cfd8aac2f121bdffd88bcd
Levin <Levin_Lisica@outlook.de>
2026-09-21 11:08:13 +0200

    Basic BuildingBlockCard

--- src/components/BuildingBlockCard.vue
+++ src/components/BuildingBlockCard.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="building-block-card">
-    <slot />
+    <slot name="image"></slot>
+
+    <div class="card-content">
+      <slot name="title"></slot>
+
+      <slot></slot>
+
+      <slot name="footer"></slot>
+    </div>
   </div>
 </template>
